ac39e4f66d4852db32993283c0f5f3a2e03a90c6
tomszociepa <tom.ociepa@gmail.com>
2025-08-03 21:11:10 +0200

    init project

---
+++ src/main/java/com/tom/ociepa/github_repo_browser/GithubRepoBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.tom.ociepa.github_repo_browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tom/ociepa/github_repo_browser/GithubRepoBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.ociepa.github_repo_browser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
